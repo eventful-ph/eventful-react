abb21cf3522878afa1618b19b2b688c207d6aa46
Daryll Magsombol <daryllmagsombol@Darylls-MacBook-Air.local>
2023-01-18 21:15:49 +0800

    added second vendor sign up page, toggleble

--- src/components/VendorScreen.tsx
+++ src/components/VendorScreen.tsx
@@ -1,11 +1,5 @@
 import React, { useState } from 'react';
-import {
-  Button,
-  Card,
-  CardContent,
-  TextField,
-  Typography,
-} from '@mui/material';
+import { Button, Card, CardContent, Typography } from '@mui/material';
 
 import './VendorScreen.css';
 import { Stack } from '@mui/system';
@@ -14,13 +8,122 @@ import Footer from './Footer/Footer';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { DatePicker, LocalizationProvider } from '@mui/x-date-pickers';
 
+type VendorPage1Type = {
+  formData: any;
+  setFormData: (v: any) => void;
+  setPage?: (page: number) => void;
+};
+
+const VendorSignupPage1 = ({
+  formData,
+  setFormData,
+  setPage = () => {},
+}: VendorPage1Type) => {
+  return (
+    <Card sx={{ minWidth: 512, minHeight: 462, textAlign: 'left' }}>
+      <CardContent>
+        <div className='vendor-form-div'>
+          <div className='tell-us-div'>
+            <Typography className='tell-us-font'>
+              Tell us about yourself
+            </Typography>
+            <Typography className='tell-us-caption' gutterBottom>
+              Briefly tell what this card can do for the user
+            </Typography>
+          </div>
+
+          <Stack component='form' spacing={2} noValidate autoComplete='off'>
+            <Textbox
+              label='First Name'
+              value={formData.firstName}
+              onChange={(v) => setFormData({ ...formData, firstName: v })}
+            />
+            <Textbox
+              label='Last Name'
+              value={formData.lastName}
+              onChange={(v) => setFormData({ ...formData, lastName: v })}
+            />
+            <Textbox
+              label='Contact Number'
+              value={formData.contactNo}
+              onChange={(v) => setFormData({ ...formData, contactNo: v })}
+            />
+            <LocalizationProvider dateAdapter={AdapterDayjs}>
+              <DatePicker
+                label='Birthdate'
+                value={formData.birthDate}
+                onChange={(v) => setFormData({ ...formData, birthDate: v })}
+                renderInput={(params: any) => <Textbox {...params} />}
+              />
+            </LocalizationProvider>
+            <Button variant='contained' onClick={() => setPage(2)}>
+              Next
+            </Button>
+          </Stack>
+        </div>
+      </CardContent>
+    </Card>
+  );
+};
+
+const VendorSignupPage2 = ({
+  formData,
+  setFormData,
+  setPage = () => {},
+}: VendorPage1Type) => {
+  return (
+    <Card sx={{ minWidth: 512, minHeight: 462, textAlign: 'left' }}>
+      <CardContent>
+        <div className='vendor-form-div'>
+          <div className='tell-us-div'>
+            <Typography className='tell-us-font'>
+              What do you offer your customers?
+            </Typography>
+            <Typography className='tell-us-caption' gutterBottom>
+              Briefly tell what this card can do for the user
+            </Typography>
+          </div>
+
+          <Stack component='form' spacing={2} noValidate autoComplete='off'>
+            <Textbox
+              label='First Name'
+              value={formData.businessName}
+              onChange={(v) => setFormData({ ...formData, businessName: v })}
+            />
+            <Textbox
+              label='Select which applies to your business'
+              value={formData.businessCategory}
+              onChange={(v) =>
+                setFormData({ ...formData, businessCategory: v })
+              }
+            />
+            <Textbox
+              label='Office Location'
+              value={formData.officeLocation}
+              onChange={(v) => setFormData({ ...formData, officeLocation: v })}
+            />
+          </Stack>
+          <Button variant='contained' onClick={() => setPage(1)}>
+            Back
+          </Button>
+          <Button variant='contained'>Next</Button>
+        </div>
+      </CardContent>
+    </Card>
+  );
+};
+
 function VendorScreen() {
   const [formData, setFormData] = useState<any>({
     firstName: '',
     lastName: '',
     contactNo: '+63',
     birthDate: null,
+    businessName: '',
+    businessCategory: '',
+    officeLocation: '',
   });
+  const [page, setPage] = useState<number>(1);
   return (
     <>
       <div className='mainVendorDiv'>
@@ -30,8 +133,6 @@ function VendorScreen() {
             src={require('../assets/images/eventful-logo-232.png')}
             alt='Eventful'
           />
-          {/* <h3 className='reg-as-vendor'>Register as a vendor</h3>
-           */}
           <div className='mainTextDiv'>
             <Typography className='reg-as-vendor' variant='h3'>
               Register as a vendor
@@ -43,61 +144,20 @@ function VendorScreen() {
               Connect with more customers
             </Typography>
           </div>
-
-          <Card sx={{ minWidth: 512, minHeight: 462, textAlign: 'left' }}>
-            <CardContent>
-              <div className='vendor-form-div'>
-                <div className='tell-us-div'>
-                  <Typography className='tell-us-font'>
-                    Tell us about yourself
-                  </Typography>
-                  <Typography
-                    // sx={{ fontSize: 14 }}
-                    className='tell-us-caption'
-                    // color='text.secondary'
-                    gutterBottom
-                  >
-                    Briefly tell what this card can do for the user
-                  </Typography>
-                </div>
-
-                <Stack
-                  component='form'
-                  spacing={2}
-                  noValidate
-                  autoComplete='off'
-                >
-                  <Textbox
-                    label='First Name'
-                    value={formData.firstName}
-                    onChange={(v) => setFormData({ ...formData, firstName: v })}
-                  />
-                  <Textbox
-                    label='Last Name'
-                    value={formData.lastName}
-                    onChange={(v) => setFormData({ ...formData, lastName: v })}
-                  />
-                  <Textbox
-                    label='Contact Number'
-                    value={formData.contactNo}
-                    onChange={(v) => setFormData({ ...formData, contactNo: v })}
-                  />
-                  <LocalizationProvider dateAdapter={AdapterDayjs}>
-                    <DatePicker
-                      label='Birthdate'
-                      value={formData.birthDate}
-                      onChange={(v) =>
-                        setFormData({ ...formData, birthDate: v })
-                      }
-                      renderInput={(params: any) => <Textbox {...params} />}
-                    />
-                  </LocalizationProvider>
-
-                  <Button variant='contained'>Next</Button>
-                </Stack>
-              </div>
-            </CardContent>
-          </Card>
+          {page === 1 && (
+            <VendorSignupPage1
+              formData={formData}
+              setFormData={setFormData}
+              setPage={setPage}
+            />
+          )}
+          {page === 2 && (
+            <VendorSignupPage2
+              formData={formData}
+              setFormData={setFormData}
+              setPage={setPage}
+            />
+          )}
         </div>
       </div>
       <Footer />
